bf6283dcd9b8e84acb478ab3cd2f5301e1d650f4
KalashnikovTV <max.malaschkov@mail.ru>
2020-11-14 21:55:08 +0300

    start adaptive

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 $(".phone-mask").mask("+375 (99) 999-99-99");
+
+$('.header__menu-btn').on('click', function () {
+    $('.menu .menu__list').slideToggle();
+});
+
+// 2-ой выпадающий список
+$('.menu__list .menu__list-wrapper .menu__list-drop-down').on('click', function() {
+    $(this).next().toggleClass('active');
+});
+
+// 3-ий выпадающий список
+$('.drop-down__list .menu-arrow').on('click', function () {
+    $(this).next().toggleClass('active');
+});
